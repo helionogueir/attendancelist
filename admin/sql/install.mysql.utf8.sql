e6ac9754406055bd7bae2c81b317162bfee7ee09
helionogueir <helio.nogueir@gmail.com>
2017-09-06 09:34:06 -0300

    Update Database Model

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -28,50 +28,113 @@ CREATE TABLE IF NOT EXISTS `#__attendancelist_category` (
 	`modified` DATETIME NOT NULL,
 	`published` TINYINT(1) NOT NULL DEFAULT '1',
     PRIMARY KEY (`id`),
-    INDEX `ind_attendancelist_category_code` (`code`),
     INDEX `ind_attendancelist_category_attendancelist_id` (`attendancelist_id`),
+    INDEX `ind_attendancelist_category_code` (`code`),
+    UNIQUE INDEX `unq_attendancelist_category_code` (`parent`, `code`),
     INDEX `ind_attendancelist_category_parent` (`parent`),
     INDEX `ind_attendancelist_category_published` (`published`)
 )
 COLLATE='utf8_unicode_ci'
 ENGINE=InnoDB;
 
-/* Attendance List Student */
-DROP TABLE IF EXISTS `#__attendancelist_student`;
-CREATE TABLE IF NOT EXISTS `#__attendancelist_student` (
+/* Attendance List Category Target */
+DROP TABLE IF EXISTS `#__attendancelist_category_target`;
+CREATE TABLE IF NOT EXISTS `#__attendancelist_category_target` (
 	`id` BIGINT(18) NOT NULL AUTO_INCREMENT,
 	`category_id` BIGINT(18) NOT NULL,
 	`code` VARCHAR(45) NOT NULL,
-	`genre` ENUM('F','M') NULL DEFAULT NULL,
-	`firstname` VARCHAR(255) NOT NULL,
-	`lastname` VARCHAR(255) NOT NULL,
-	`email` VARCHAR(255) NOT NULL,
+	`title` VARCHAR(255) NOT NULL,
 	`obs` TEXT NULL,
 	`created` DATETIME NOT NULL,
 	`modified` DATETIME NOT NULL,
 	`published` TINYINT(1) NOT NULL DEFAULT '1',
     PRIMARY KEY (`id`),
-    INDEX `ind_attendancelist_student_code` (`code`),
-    INDEX `ind_attendancelist_category_category_id` (`category_id`),
-    INDEX `ind_attendancelist_student_genre` (`genre`),
-    INDEX `ind_attendancelist_student_published` (`published`)
+    INDEX `ind_attendancelist_category_target_id` (`category_id`),
+    INDEX `ind_attendancelist_category_target_code` (`code`),
+    UNIQUE INDEX `unq_attendancelist_category_target_code` (`category_id`, `code`),
+    INDEX `ind_attendancelist_category_target_published` (`published`)
 )
 COLLATE='utf8_unicode_ci'
 ENGINE=InnoDB;
 
-/* Attendance List Student Attendance */
-DROP TABLE IF EXISTS `#__attendancelist_student_attendance`;
-CREATE TABLE IF NOT EXISTS `#__attendancelist_student_attendance` (
+/* Attendance List Quiz */
+DROP TABLE IF EXISTS `#__attendancelist_quiz`;
+CREATE TABLE IF NOT EXISTS `#__attendancelist_quiz` (
+	`id` BIGINT(18) NOT NULL AUTO_INCREMENT,
+	`attendancelist_id` BIGINT(18) NOT NULL,
+	`type` VARCHAR(25) NOT NULL,
+	`question` TEXT NOT NULL,
+	`created` DATETIME NOT NULL,
+	`modified` DATETIME NOT NULL,
+	`published` TINYINT(1) NOT NULL DEFAULT '1',
+    PRIMARY KEY (`id`),
+    INDEX `ind_attendancelist_quiz_attendancelist_id` (`attendancelist_id`),
+    INDEX `ind_attendancelist_quiz_type` (`type`)
+)
+COLLATE='utf8_unicode_ci'
+ENGINE=InnoDB;
+
+/* Attendance List Quiz Alternative */
+DROP TABLE IF EXISTS `#__attendancelist_quiz_alternative`;
+CREATE TABLE IF NOT EXISTS `#__attendancelist_quiz_alternative` (
+	`id` BIGINT(18) NOT NULL AUTO_INCREMENT,
+	`quiz_id` BIGINT(18) NOT NULL,
+	`alternative` TEXT NOT NULL,
+	`created` DATETIME NOT NULL,
+	`modified` DATETIME NOT NULL,
+	`published` TINYINT(1) NOT NULL DEFAULT '1',
+    PRIMARY KEY (`id`),
+    INDEX `ind_attendancelist_quiz_quiz_id` (`quiz_id`)
+)
+COLLATE='utf8_unicode_ci'
+ENGINE=InnoDB;
+
+/* Attendance List Feedback */
+DROP TABLE IF EXISTS `#__attendancelist_feedback`;
+CREATE TABLE IF NOT EXISTS `#__attendancelist_feedback` (
 	`id` BIGINT(18) NOT NULL AUTO_INCREMENT,
 	`user_id` INT(11) NOT NULL,
-	`student_id` BIGINT(18) NOT NULL,
-	`present` ENUM('0','1') NOT NULL,
+	`date` DATE NOT NULL,
+	`timestart` TIME NULL,
+	`timefinish` TIME NULL,
+	`title` VARCHAR(255) NULL,
 	`created` DATETIME NOT NULL,
+	`modified` DATETIME NOT NULL,
+	`published` TINYINT(1) NOT NULL DEFAULT '1',
     PRIMARY KEY (`id`),
-    INDEX `ind_attendancelist_student_attendance_user_id` (`user_id`),
-    INDEX `ind_attendancelist_student_attendance_student_id` (`student_id`),
-    INDEX `ind_attendancelist_student_attendance_present` (`present`),
-    INDEX `ind_attendancelist_student_attendance_created` (`created`)
+    INDEX `ind_attendancelist_feedback_user_id` (`user_id`),
+    INDEX `ind_attendancelist_feedback_date` (`date`),
+    INDEX `ind_attendancelist_feedback_timestart` (`timestart`),
+    INDEX `ind_attendancelist_feedback_timefinish` (`timefinish`),
+    INDEX `ind_attendancelist_feedback_published` (`published`)
+)
+COLLATE='utf8_unicode_ci'
+ENGINE=InnoDB;
+
+/* Attendance List Feedback Quiz */
+DROP TABLE IF EXISTS `#__attendancelist_feedback_quiz`;
+CREATE TABLE IF NOT EXISTS `#__attendancelist_feedback_quiz` (
+	`id` BIGINT(18) NOT NULL AUTO_INCREMENT,
+	`feedback_id` BIGINT(18) NOT NULL,
+	`question` TEXT NOT NULL,
+	`answer` TEXT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `ind_attendancelist_feedback_quiz_feedback_id` (`feedback_id`)
+)
+COLLATE='utf8_unicode_ci'
+ENGINE=InnoDB;
+
+/* Attendance List Feedback Presence */
+DROP TABLE IF EXISTS `#__attendancelist_feedback_presence`;
+CREATE TABLE IF NOT EXISTS `#__attendancelist_feedback_presence` (
+	`id` BIGINT(18) NOT NULL AUTO_INCREMENT,
+	`feedback_id` BIGINT(18) NOT NULL,
+	`target_id` BIGINT(18) NOT NULL,
+	`present` ENUM('0','1') NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `ind_attendancelist_feedback_presence_feedback_id` (`feedback_id`),
+    INDEX `ind_attendancelist_feedback_presence_target_id` (`target_id`),
+    INDEX `ind_attendancelist_feedback_presence_present` (`present`)
 )
 COLLATE='utf8_unicode_ci'
 ENGINE=InnoDB;
--- admin/sql/uninstall.mysql.utf8.sql
+++ admin/sql/uninstall.mysql.utf8.sql
@@ -4,8 +4,20 @@ DROP TABLE IF EXISTS `#__attendancelist`;
 /* Attendance List Category */
 DROP TABLE IF EXISTS `#__attendancelist_category`;
 
-/* Attendance List Student */
-DROP TABLE IF EXISTS `#__attendancelist_student`;
+/* Attendance List Category Target */
+DROP TABLE IF EXISTS `#__attendancelist_category_target`;
 
-/* Attendance List Student Attendance */
-DROP TABLE IF EXISTS `#__attendancelist_student_attendance`;
+/* Attendance List Quiz */
+DROP TABLE IF EXISTS `#__attendancelist_quiz`;
+
+/* Attendance List Quiz Alternative */
+DROP TABLE IF EXISTS `#__attendancelist_quiz_alternative`;
+
+/* Attendance List Feedback */
+DROP TABLE IF EXISTS `#__attendancelist_feedback`;
+
+/* Attendance List Feedback Quiz */
+DROP TABLE IF EXISTS `#__attendancelist_feedback_quiz`;
+
+/* Attendance List Feedback Presence */
+DROP TABLE IF EXISTS `#__attendancelist_feedback_presence`;
